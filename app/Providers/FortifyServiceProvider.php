4594e09d5a236335211dea65aef0c0cb88d75e6c
andrius <andriusgeci@gmail.com>
2023-09-12 18:42:27 +0100

    1. added laravel fortify reset password functionality

--- app/Providers/FortifyServiceProvider.php
+++ app/Providers/FortifyServiceProvider.php
@@ -48,6 +48,14 @@ class FortifyServiceProvider extends ServiceProvider
             }
         });
 
+        Fortify::requestPasswordResetLinkView(function () {
+            return view('auth.forgot-password');
+        });
+
+        Fortify::resetPasswordView(function (Request $request) {
+            return view('auth.reset-password', ['request' => $request]);
+        });
+
         Fortify::createUsersUsing(CreateNewUser::class);
         Fortify::updateUserProfileInformationUsing(UpdateUserProfileInformation::class);
         Fortify::updateUserPasswordsUsing(UpdateUserPassword::class);
--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -3,29 +3,22 @@
 @section('content')
     <div class="row mt-5 pt-5">
         <div class="col-md-12">
-            <h3>Register to Laracroft</h3>
+            <h3>Reset your password</h3>
             <div class="card">
                 <div class="card-header bg-secondary">
-                    <h5 class="text-center text-light">Register</h5>
+                    <h5 class="text-center text-light">Reset Password</h5>
                 </div>
                 <div class="card-body">
-                    <form action="{{ route('register') }}" method="POST">
+                    <form action="{{ url('reset-password') }}" method="POST">
                         @csrf
+
+                        <input type="hidden" name="token" value="{{ $request->token }}">
+
                         <div class="form-group my-1">
                             <label for="email">Email</label>
                             <input type="email" name="email" class="form-control @error('email') is-invalid
                                    @enderror" value="{{ old('email') }}" placeholder="Email Address" required>
                             @error('email')
-                            <span class="invalid-feedback" role="alert">
-                                <strong>{{ $message }}</strong>
-                            </span>
-                            @enderror
-                        </div>
-                        <div class="form-group my-1">
-                            <label for="name">Name</label>
-                            <input type="text" name="name" class="form-control @error('name') is-invalid @enderror"
-                                   value="{{ old('name') }}" placeholder="Name" required>
-                            @error('name')
                             <span class="invalid-feedback" role="alert">
                                 <strong>{{ $message }}</strong>
                             </span>
@@ -56,11 +49,8 @@
                         <div class="row">
                             <div class="col md-6">
                                 <div class="form-group my-1">
-                                    <button type="submit" class="btn btn-warning float-start">Register</button>
+                                    <button type="submit" class="btn btn-warning float-start">Reset Password</button>
                                 </div>
-                            </div>
-                            <div class="div col md-6 my-1">
-                                <a href="{{ route('login') }}" class="btn btn-dark float-end">Go to login</a>
                             </div>
                         </div>
                     </form>
